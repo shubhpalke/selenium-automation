341124f1874e8a09a2d291c9600834a6bb153fc3
Shubham Palke <shubhpalke@gmail.com>
2026-08-23 13:54:09 +0530

    Initial commit - Selenium automation framework

---
+++ src/test/java/com/qa/selenium_automation/AppTest.java
@@ -0,0 +1,16 @@
+package com.qa.selenium_automation;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
