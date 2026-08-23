db3e13a29b563bbe14b8655bbda53c108d669b57
Shubham Palke <shubhpalke@gmail.com>
2026-08-23 14:18:42 +0530

    Test 2 commit

--- src/main/java/com/qa/selenium_automation/App.java
+++ src/main/java/com/qa/selenium_automation/App.java
@@ -6,5 +6,6 @@ package com.qa.selenium_automation;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
